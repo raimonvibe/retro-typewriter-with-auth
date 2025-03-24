c908dc4396effc03ffc4e114380a86ca25851eea
raimonvibe <robertjanstefan@gmail.com>
2025-03-24 15:23:43 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import { AuthProvider } from "@/components/auth-provider"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
